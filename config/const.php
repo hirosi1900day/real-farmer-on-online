67f8664af0cbe333fa28191cda16eef3d1831c12
jiro.kirameki <ubuntu@ip-172-31-25-132.ec2.internal>
2021-02-07 03:55:19 +0000

    complete

--- config/const.php
+++ config/const.php
@@ -2,5 +2,5 @@
 return['mypage_informations'=>[['user.pay.index','ポイントを追加する'],['user.instruction','指示を追加する'],
         ['user.field','畑を追加する'],['user.plant','植物を追加する'],
         ['user.daily.index', '日記を見る'],['community.view','コミュニティーチャット'],['user_request.create','要望をする'],['user.myfield','自分の畑の情報を見る']],
-        'admin_user'=>['realFarmerAdmin@email.com','realFarmerAdmin'],
+        'admin_user'=>['sugashi1900day@gmail.com','admin_user'],
         'mypage_image'=>['/img/point.jpg','/img/instruction.jpg','/img/field.jpg','/img/plant.jpg','/img/daily.jpg','/img/comunitychat.jpg','/img/request.jpg','/img/chat.jpg','/img/myfi.jpg','/img/plant.jpg']];
