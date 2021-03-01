30af55f93e5bdfdcfcfa380e9dd7cc0ce304cea9
jiro.kirameki <ubuntu@ip-172-31-25-132.ec2.internal>
2021-03-01 12:51:23 +0000

    mypage fix

--- config/const.php
+++ config/const.php
@@ -1,6 +1,6 @@
 <?php 
-return['mypage_informations'=>[['user.pay.index','ポイントを追加する'],['user.instruction','指示を追加する'],
-        ['user.field','畑を追加する'],['user.plant','植物を追加する'],
+return['mypage_informations'=>[['user.pay.index','ポイントを追加する(STEP1)'],
+        ['user.field','畑を追加する(STEP2)'],['user.plant','植物を追加する(STEP3)'],['user.instruction','指示を追加する(STEP4)'],
         ['user.daily.index', '日記を見る'],['community.view','コミュニティーチャット'],['user_request.create','要望をする'],['user.myfield','自分の畑の情報を見る']],
         'admin_user'=>['sugashi1900day@gmail.com','admin_user'],
-        'mypage_image'=>['/img/point.jpg','/img/instruction.jpg','/img/field.jpg','/img/plant.jpg','/img/daily.jpg','/img/comunitychat.jpg','/img/request.jpg','/img/chat.jpg','/img/myfi.jpg','/img/plant.jpg']];
+        'mypage_image'=>['/img/point.jpg','/img/field.jpg','/img/plant.jpg','/img/instruction.jpg','/img/daily.jpg','/img/comunitychat.jpg','/img/request.jpg','/img/chat.jpg','/img/myfi.jpg','/img/plant.jpg']];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>Microposts</title>
+        <title>realfarmer</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content"{{csrf_token()}}">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
